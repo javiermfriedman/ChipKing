594892e43ea829f5e5a744458f9ae63a1a17689a
javiermfriedman <147084311+javiermfriedman@users.noreply.github.com>
2024-08-11 15:09:34 -0400

    Initial Commit

---
+++ Kingpin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kingpin
+//
+//  Created by Javier Friedman on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kingpin/KingpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingpinApp.swift
+//  Kingpin
+//
+//  Created by Javier Friedman on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KingpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
